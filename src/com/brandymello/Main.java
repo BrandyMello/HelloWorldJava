9769f7c33f6b16f52fdc9949ac10ac206293b154
Brandy Mello <brandyamello@gmail.com>
2020-01-02 15:26:19 -0700

    Work through Strings in tutorial

--- src/com/brandymello/Main.java
+++ src/com/brandymello/Main.java
@@ -1,8 +1,50 @@
 package com.brandymello;
 
+import java.awt.*;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World!");
+//        int age = 30;
+//        age = 35;
+//        byte myAge = 30;
+//        short herAge = myAge;
+//        Date now = new Date();
+//        System.out.println(now);
+////        now.getTime();
+//        long viewCount = 3_123_456_789L;
+//        double oldPrice = 9.99;
+//        float price = 10.99F;
+//        char letter = 'A';
+//        boolean isEligible = false;
+//	    System.out.println("Hello World!");
+//	    System.out.println(age);
+//        System.out.println(herAge);
+//        byte x = 1;
+//        byte y = x;
+//        x = 2;
+////        System.out.println(y);
+//        Point point1 = new Point(1,1);
+//        Point point2 = point1;
+//        point1.x = 2;
+//        System.out.println(point2);
+//        String message = "Hello World" + "!!";
+//        System.out.println(message.endsWith("!!"));
+//        System.out.println(message.startsWith("!!"));
+//        System.out.println(message.length());
+//        System.out.println(message.indexOf("e"));
+//        System.out.println(message.indexOf("sky"));
+//        System.out.println(message.replace("!", "*"));
+//        System.out.println(message);
+//        System.out.println(message.toUpperCase());
+//        System.out.println(message.toLowerCase());
+//        System.out.println(message.trim());
+//        String nameMessage = "Hello \"Mosh\"";
+//        System.out.println(nameMessage);
+//        String brokenMessage = "Hello \nMosh";
+//        String tabbedMessage = "Hello \tMosh";
+//        System.out.println(brokenMessage);
+//        System.out.println(tabbedMessage);
     }
 }
