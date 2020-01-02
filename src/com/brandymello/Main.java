77411c8c0ad6337a0d5397449b07dbc0c5406882
Brandy Mello <brandyamello@gmail.com>
2020-01-02 16:45:03 -0700

    Work through arrays casting type diversion and Math class

--- src/com/brandymello/Main.java
+++ src/com/brandymello/Main.java
@@ -1,11 +1,85 @@
 package com.brandymello;
 
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
+        // Work through Arrays and ArrayList
+//        int[] numbers = new int[5];
+//        numbers[0] = 1;
+//        numbers[2] = 2;
+////        numbers[10] = 3;
+////        Arrays.toString(numbers);
+//        System.out.println(Arrays.toString(numbers));
+//        int[] nums = {2,3,5,1,4};
+//        Arrays.sort(nums);
+//        System.out.println(nums);
+//        System.out.println(Arrays.toString(nums));
+//
+//        //multi dimensional arrays
+//        int[][] multiDimeNums = new int[2][3];
+//        multiDimeNums[0][0] = 1;
+//        System.out.println(Arrays.deepToString(multiDimeNums));
+//
+//        int[][] doubleDime = { { 1, 2, 3}, { 2, 4, 6} };
+//        System.out.println(Arrays.deepToString(doubleDime));
+//
+//
+//        //final treats variable as constant; cannot change; use all caps for name
+//        final float PI = 3.14F;
+
+//        //Arithmetic expressions
+//        int result = 10 + 3;
+//        double divResult = (double)10 / (double)3;
+//        int x = 1;
+//        //x++; || ++x; If you put the incrementer before the variable when assigning it to a new variable, it will increment before the assignment verses being incremented after.
+//        int y = ++x;
+//        // x += 2, x -=2, x /= 2, x *= 2
+//        // multiplication and division take higher priority in order of operation just like in math
+//        System.out.println(result);
+//        System.out.println(divResult);
+//        System.out.println(x);
+//        System.out.println(y);
+
+        //Casting and Type diversion: implicit casting - converts to the bigger(byte) type or more precise
+//        short x = 1;
+//        int y = x + 2;
+//        System.out.println(y);
+//        //Explicit casting (only with compatible types):
+//        //byte > short > int > long > float > double
+//        double b = 3.4;
+//        int c = (int)b + 2;
+//        System.out.println(c);
+//        //ParseInt
+//        String d = "5";
+////        Integer.parseInt() //wrapper class for the int primitive type...
+//        int f = Integer.parseInt(d) + 2;
+//        System.out.println(f);
+//        String g = "5.7";
+//        float h = Float.parseFloat(g) + 3;
+//        System.out.println(h);
+
+//        //Math class
+//        int result = Math.round(1.1F); //or float result = Math.round(1.1) for 1.0 returned;
+//        System.out.println(result);
+//        int ceilResult = (int)Math.ceil(1.1F);
+//        System.out.println(ceilResult);
+//        int floorResult = (int)Math.floor(1.1F);
+//        System.out.println(floorResult);
+//        int maxResult = Math.max(1, 4);
+//        System.out.println(maxResult);
+//        double randomResult = Math.random();
+//        System.out.println(randomResult);
+//        int randomResult2 = (int)Math.round(Math.random() * 100); // or int randomResult2 = (int)(Math.random() * 100);
+//        System.out.println(randomResult2);
+
+        //Formatting Numbers
+
+        //Work through Strings:
+
 //        int age = 30;
 //        age = 35;
 //        byte myAge = 30;
